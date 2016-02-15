80414c6bd7ff3cf567839884de11ae770c937b22
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2016-02-15 13:20:27 +0200

    add record compatibility tests

--- tests/FSharp.DynamoDB.Tests/SimpleTableOperationTests.fs
+++ tests/FSharp.DynamoDB.Tests/SimpleTableOperationTests.fs
@@ -11,14 +11,17 @@ open FSharp.DynamoDB
 type SimpleRecord =
     {
         [<HashKey>]
-        Name : string
+        HashKey : string
+        [<RangeKey>]
+        RangeKey : string
 
         Value : int64
     }
 
+[<HashKeyConstant("HashKey", "compatible")>]
 type CompatibleRecord =
     {
-        [<HashKey; CustomName("Name")>]
+        [<RangeKey; CustomName("RangeKey")>]
         Id : string
 
         Values : int list
@@ -29,37 +32,43 @@ type ``Simple Table Operation Tests`` () =
     let client = getDynamoDBAccount()
     let tableName = getRandomTableName()
 
+    let mkItem() = { HashKey = guid() ; RangeKey = guid() ; Value = int64 <| Random().Next() }
+
     let run = Async.RunSynchronously
 
     let table = TableContext.GetTableContext<SimpleRecord>(client, tableName, createIfNotExists = true) |> run
 
     [<Fact>]
+    let ``Convert to compatible table`` () =
+        let table' = table.WithRecordType<CompatibleRecord> ()
+        table'.KeySchema |> should equal table.KeySchema
+
+    [<Fact>]
     let ``Simple Put Operation`` () =
-        let value = { Name = guid() ; Value = 42L }
+        let value = mkItem()
         let key = table.PutItemAsync value |> run
         let value' = table.GetItemAsync key |> run
         value' |> should equal value
 
     [<Fact>]
     let ``ContainsKey Operation`` () =
-        let value = { Name = guid() ; Value = 42L }
+        let value = mkItem()
         let key = table.PutItemAsync value |> run
         table.ContainsKeyAsync key |> run |> should equal true
 
     [<Fact>]
     let ``Batch Put Operation`` () =
-        let values = set [ for i in 1L .. 20L -> { Name = guid() ; Value = i } ]
+        let values = set [ for i in 1L .. 20L -> mkItem() ]
         let keys = table.PutItemsAsync values |> run
         let values' = table.GetItemsAsync keys |> run |> Set.ofArray
         values' |> should equal values
 
     [<Fact>]
     let ``Simple Delete Operation`` () =
-        let item = { Name = guid() ; Value = 0L }
+        let item = mkItem()
         let key = table.PutItemAsync item |> run
         table.ContainsKeyAsync key |> run |> should equal true
         table.DeleteItemAsync key |> run
-        Thread.Sleep 5000
         table.ContainsKeyAsync key |> run |> should equal false
 
     interface IDisposable with
